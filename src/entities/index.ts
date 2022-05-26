60e54dae97004bd9b144d1de9261b92892b4b8f5
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-26 13:56:10 -0300

    feat: created index to entities

---
+++ src/entities/index.ts
@@ -0,0 +1,6 @@
+import { User } from "./user.entity";
+import { Cart } from "./cart.entity";
+import { Dvd } from "./dvd.entity";
+import { Stock } from "./stock.entity";
+
+export { User, Cart, Dvd, Stock };
