9ad1340d4ed1361cff6b3c36f4f0c780721dfc8c
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-26 10:28:42 -0300

    feat: create users table
    1 - Created User entity.
    2 - Created sripts to optimize creation, generation and execution of
    migrations.
    3 - Created, generate a run migrations to create migrations and users
      tables.
    4 - Fix join method in data-source-ts

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false, unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ type: 'boolean', default: false })
+  isAdmin: boolean;
+}
---
+++ src/migrations/1653571398081-createMigrationsTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class createMigrationsTable1653571398081 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
