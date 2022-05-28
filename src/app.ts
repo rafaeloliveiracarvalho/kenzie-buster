7ee7c9fcd37396744b915998f5485f8962cdb539
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-28 14:14:21 -0300

    feat: custom error
    1 - Created custom error named ErrorHandler.
    2 - Created function errorHandler to capture any error thats is instance from ErrorHandler and applied in app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { errorHandler } from "./errors";
 import registerRoutes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 registerRoutes(app);
+app.use((error: Error, _: Request, res: Response, __: NextFunction) => {
+  return errorHandler(error, res);
+});
 
 export default app;
