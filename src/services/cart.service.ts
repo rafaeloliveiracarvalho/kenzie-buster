161cdebcb67646830f79b9c0ca699fd93a4204b9
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-30 17:01:53 -0300

    fix: resolve bugs to add products in new cart

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -1,17 +1,40 @@
 import { Request } from "express";
+import { User } from "../entities";
 import { IAddDvdInCart, IUserDecoded } from "../interfaces";
 import { cartRepo } from "../repositories";
 
 class CartService {
   addDvdInCart = async ({ dvd, decoded, validated }: Request) => {
     const { quantity } = validated as IAddDvdInCart;
-    const { iat, exp, ...user } = decoded as IUserDecoded;
+    const { exp, iat, ...user } = decoded as IUserDecoded;
     const total = +(quantity * dvd.stock.price).toFixed(2);
 
-    const newCart = await cartRepo.createCart({ dvd, user, total });
+    // const noCardPaid = await this.getNoPaidCart({ decoded } as Request);
+
+    // console.log("\n\n\n");
+    // console.log("noCardPaid", noCardPaid);
+    // console.log("\n\n\n");
+
+    // if (noCardPaid) {
+    // }
+
+    const newCart = await cartRepo.createCart({
+      total,
+      customer: { ...user },
+      dvds: [dvd],
+    });
 
     return newCart;
   };
+
+  getNoPaidCart = async ({ decoded }: Request) => {
+    const { id } = decoded;
+    const cart = cartRepo.getNoPaidCart({ customer: { id }, paid: false });
+
+    return cart;
+  };
+
+  updateCart = async () => {};
 }
 
 export default new CartService();
