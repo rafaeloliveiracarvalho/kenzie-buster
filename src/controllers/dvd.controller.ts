dd5874fda52bbc349fae2faa17cf637d400468be
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-06-03 18:03:05 -0300

    fix: added response examplos in doc

--- src/controllers/dvd.controller.ts
+++ src/controllers/dvd.controller.ts
@@ -5,7 +5,7 @@ class DvdController {
   createDvd = async (req: Request, res: Response) => {
     const newDvds = await dvdService.createDvd(req);
 
-    res.status(200).json({ dvds: newDvds });
+    res.status(201).json({ dvds: newDvds });
   };
 
   getAllDvds = async (_: Request, res: Response) => {
