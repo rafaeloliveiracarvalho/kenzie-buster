bee2a7327b10072e2af5a1cceea9d5ef5b01342b
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-06-03 23:11:59 -0300

    feat: enabled cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+
 import { errorHandler } from "./errors";
 import registerRoutes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 registerRoutes(app);
+
 app.use((error: Error, _: Request, res: Response, __: NextFunction) => {
   return errorHandler(error, res);
 });
