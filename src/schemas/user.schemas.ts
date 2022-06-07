192842576b22bc32369d938d653f65f6c13979d9
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-06-07 13:40:32 -0300

    update: change 'isAdmin' to 'isAdm'

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -4,7 +4,7 @@ export const createUserSchema = yup.object().shape({
   name: yup.string().required("name is a required field"),
   email: yup.string().email().required("email is a required field"),
   password: yup.string().required("password is a required field"),
-  isAdmin: yup.boolean().default(false).optional(),
+  isAdm: yup.boolean().default(false).optional(),
 });
 
 export const loginUserSchema = yup.object().shape({
@@ -16,5 +16,5 @@ export const serializedUserSchema = yup.object().shape({
   id: yup.string().uuid().required(),
   name: yup.string().required("name is a required field"),
   email: yup.string().email().required("email is a required field"),
-  isAdmin: yup.boolean().default(false).optional(),
+  isAdm: yup.boolean().default(false).optional(),
 });
